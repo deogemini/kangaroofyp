2059eac46c2044b63feed169ef336fdbcf440c74
deogemini <geminideogratias8@gmail.com>
2021-04-18 20:46:30 +0300

    second commit

---
+++ app/src/main/java/com/hfad/kangaroo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.hfad.kangaroo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+//    Button button = (Button) findViewById(R.id.button2);
+//button.setOnClickListener(new View.OnClickListener() {
+//        public void onClick(View v) {
+//            // Do something in response to button click
+//        }
+//    });
+}
